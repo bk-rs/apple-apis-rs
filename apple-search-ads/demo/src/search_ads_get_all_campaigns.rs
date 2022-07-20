ec3ae0d75eee8ebb65cb471b8664ba209c180e77
vkill <vkill.net@gmail.com>
2022-07-20 16:38:42 +0800

    Refactor demos
    Refactor demos
    
    Refactor demos

--- apple-search-ads/demo/src/search_ads_get_all_campaigns.rs
+++ apple-search-ads/demo/src/search_ads_get_all_campaigns.rs
@@ -1,10 +1,12 @@
 /*
-cargo run -p appleapis-demo-isahc --bin search_ads_get_user_acl -- demo_certs/search_ads.pem demo_certs/search_ads.key
+cargo run -p apple-search-ads-demo --bin search_ads_get_all_campaigns -- demo_certs/search_ads.pem demo_certs/search_ads.key ORG_ID
 */
 
 use std::{env, error};
 
-use apple_search_ads::v3::endpoints::get_user_acl::GetUserAcl;
+use apple_search_ads::v3::{
+    endpoints::get_all_campaigns::GetAllCampaigns, objects::pagination::Pagination,
+};
 use futures_lite::future::block_on;
 use http_api_isahc_client::{
     isahc::{
@@ -23,8 +25,12 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 async fn run() -> Result<(), Box<dyn error::Error>> {
     let cert_pem_file_path = env::args().nth(1).unwrap();
     let private_key_pem_file_path = env::args().nth(2).unwrap();
+    let org_id: u64 = env::args().nth(3).unwrap().parse().unwrap();
 
-    let get_user_acl = GetUserAcl::new();
+    let mut pagination = Pagination::new();
+    pagination.set_limit(1000);
+    let mut get_all_campaigns = GetAllCampaigns::new(org_id);
+    get_all_campaigns.set_pagination(pagination);
 
     let http_client = HttpClient::builder()
         .ssl_client_certificate(ClientCertificate::pem_file(
@@ -34,7 +40,8 @@ async fn run() -> Result<(), Box<dyn error::Error>> {
         .build()?;
     let isahc_client = IsahcClient::with(http_client);
 
-    let (response_body, response_status) = isahc_client.respond_endpoint(&get_user_acl).await?;
+    let (response_body, response_status) =
+        isahc_client.respond_endpoint(&get_all_campaigns).await?;
 
     println!("{:?}", response_body);
     println!("{:?}", response_status);
--- apple-search-ads/demo/src/search_ads_get_user_acl.rs
+++ apple-search-ads/demo/src/search_ads_get_user_acl.rs
@@ -1,12 +1,10 @@
 /*
-cargo run -p appleapis-demo-isahc --bin search_ads_get_all_campaigns -- demo_certs/search_ads.pem demo_certs/search_ads.key ORG_ID
+cargo run -p apple-search-ads-demo --bin search_ads_get_user_acl -- demo_certs/search_ads.pem demo_certs/search_ads.key
 */
 
 use std::{env, error};
 
-use apple_search_ads::v3::{
-    endpoints::get_all_campaigns::GetAllCampaigns, objects::pagination::Pagination,
-};
+use apple_search_ads::v3::endpoints::get_user_acl::GetUserAcl;
 use futures_lite::future::block_on;
 use http_api_isahc_client::{
     isahc::{
@@ -25,12 +23,8 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 async fn run() -> Result<(), Box<dyn error::Error>> {
     let cert_pem_file_path = env::args().nth(1).unwrap();
     let private_key_pem_file_path = env::args().nth(2).unwrap();
-    let org_id: u64 = env::args().nth(3).unwrap().parse().unwrap();
 
-    let mut pagination = Pagination::new();
-    pagination.set_limit(1000);
-    let mut get_all_campaigns = GetAllCampaigns::new(org_id);
-    get_all_campaigns.set_pagination(pagination);
+    let get_user_acl = GetUserAcl::new();
 
     let http_client = HttpClient::builder()
         .ssl_client_certificate(ClientCertificate::pem_file(
@@ -40,8 +34,7 @@ async fn run() -> Result<(), Box<dyn error::Error>> {
         .build()?;
     let isahc_client = IsahcClient::with(http_client);
 
-    let (response_body, response_status) =
-        isahc_client.respond_endpoint(&get_all_campaigns).await?;
+    let (response_body, response_status) = isahc_client.respond_endpoint(&get_user_acl).await?;
 
     println!("{:?}", response_body);
     println!("{:?}", response_status);
